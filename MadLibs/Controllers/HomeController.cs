029780c9ec837ecb58f2468221957c70946a3cc1
chynnalew <chynnalew@yahoo.com>
2021-09-27 14:02:50 -0700

    update readme, add nurse madlib, add splashpage
    Co-authored-by: Kevin Funez <kevin.funez315@gmail.com>

--- MadLibs/Controllers/HomeController.cs
+++ MadLibs/Controllers/HomeController.cs
@@ -5,11 +5,14 @@ namespace MadLibs.Controllers
 {
   public class HomeController : Controller
   {
-    [Route("/")]
-    public ActionResult Form() {return View();}
+    [Route ("/")]
+    public ActionResult Home() {return View();}
+    
+    [Route("/pbform")]
+    public ActionResult PBForm() {return View();}
 
-    [Route("/madlib")]
-    public ActionResult MadLib(string color1, string noun1, string food1, string adj1, string adj2, string num1, string noun2, string verb1, string verb2, string noun3) 
+    [Route("/pbmadlib")]
+    public ActionResult PBMadLib(string color1, string noun1, string food1, string adj1, string adj2, string num1, string noun2, string verb1, string verb2, string noun3) 
     { 
       Words userWords = new Words();
       userWords.Color1 = color1;
@@ -24,6 +27,29 @@ namespace MadLibs.Controllers
 
       return View(userWords); 
     }
+
+    [Route("/nurseform")]
+    public ActionResult NurseForm() {return View();}
+
+    [Route("/nursemadlib")]
+    public ActionResult NurseMadLib(string silly1, string lname, string illness, string nouns, string adj1, string adj2, string silly2, string place, string num1, string adj3) 
+    { 
+      Words userWords = new Words();
+      userWords.Silly1 = silly1;
+      userWords.Lname = lname;
+      userWords.Illness = illness;
+      userWords.Nouns = nouns;
+      userWords.Adj1 = adj1;
+      userWords.Adj2 = adj2;
+      userWords.Silly2 = silly2;
+      userWords.Place = place;
+      userWords.Num1 = num1;
+      userWords.Adj3 = adj3;
+
+
+      return View(userWords); 
+    }
+
 
     // add additional paths with the format:
   
--- MadLibs/Models/Words.cs
+++ MadLibs/Models/Words.cs
@@ -4,15 +4,22 @@ namespace MadLibs.Models
 {
   public class Words
   {
-    public string Color1 {get; set;}
-    public string Noun1 {get; set;}
-    public string Food1 {get; set;}
     public string Adj1 {get; set;}
     public string Adj2 {get; set;}
-    public string Num1 {get; set;}
+    public string Adj3 {get; set;}
+    public string Color1 {get; set;}
+    public string Food1 {get; set;}
+    public string Illness {get; set;}
+    public string Lname {get; set;}
+    public string Noun1 {get; set;}
     public string Noun2 {get; set;}
+    public string Noun3 {get; set;}
+    public string Nouns {get; set;}
+    public string Num1 {get; set;}
+    public string Place {get; set;}
+    public string Silly1 {get; set;}
+    public string Silly2 {get; set;}
     public string Verb1 {get; set;}
     public string Verb2 {get; set;}
-    public string Noun3 {get; set;}
   }
 }
